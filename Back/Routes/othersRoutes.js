0f56dda4ae805f46a92844edb73e77194fc7c4c5
MtG <119689912+mpgzz@users.noreply.github.com>
2025-05-25 15:26:32 -0600

    Delete Back directory

--- Back/Routes/othersRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { authenticateJWT, getUserData } = require('../Controllers/othersCrud');  
-
-const router = express.Router(); 
-
-router.get('/data', authenticateJWT, getUserData);
-
-module.exports = router;
